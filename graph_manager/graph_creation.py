6b2df7ee1c731b883b23c095282f4c45b45af2bb
NoamTzubery <animationkids.noam@gmail.com>
2024-11-06 13:19:33 +0200

    initial commit of basic file organization and classes

---
+++ graph_manager/graph_creation.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+
+
+class GraphManager:
+    def __init__(self, num_routers, num_switches, num_computers):
+        self.num_routers = num_routers
+        self.num_switches = num_switches
+        self.num_computers = num_computers
+        self.graph = self.create_optimized_topology()
+
+    def create_optimized_topology(self):
+        G = nx.Graph()
+
+        # Add routers, switches, and computers to the graph
+        routers = [f'Router_{i}' for i in range(self.num_routers)]
+        switches = [f'Switch_{i}' for i in range(self.num_switches)]
+        computers = [f'Computer_{i}' for i in range(self.num_computers)]
+
+        G.add_nodes_from(routers)
+        G.add_nodes_from(switches)
+        G.add_nodes_from(computers)
+
+        return G
